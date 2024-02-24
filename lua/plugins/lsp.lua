5cf546fbca73de549a5b3aec9d6820e90ea34a09
Sudeep Sharma <sharma.sudip1122@gmail.com>
2024-02-24 10:16:44 +0545

    Some modifications

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,23 +32,27 @@ return {
       "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
       "L3MON4D3/LuaSnip",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
       local cmp = require('cmp')
 
       cmp.setup({
         sources = {
-          {name = 'nvim_lsp'},
+          { name = 'nvim_lsp' },
         },
         mapping = cmp.mapping.preset.insert({
           -- Enter key confirms completion item
-          ['<CR>'] = cmp.mapping.confirm({select = false}),
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
           -- Ctrl + space triggers completion menu
           ['<C-Space>'] = cmp.mapping.complete(),
         }),
         snippet = {
           expand = function(args)
             require('luasnip').lsp_expand(args.body)
+            require("luasnip.loaders.from_vscode").lazy_load()
+            require('luasnip').filetype_extend("javascript", { "javascriptreact" })
+            require('luasnip').filetype_extend("javascript", { "html" })
           end,
         },
       })
